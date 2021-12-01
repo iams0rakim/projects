04f73f97950b596ce49a03150316388effb2fb78
iams0rakim <86761940+iams0rakim@users.noreply.github.com>
2021-12-01 20:49:11 +0900

    Add files via upload

--- PCA.R
+++ PCA.R
@@ -16,3 +16,25 @@ head(iris.pca$x[,1:2], 10)
 install.packages('ggfortify')
 library(ggfortify)
 autoplot(iris.pca, data = iris, colour = 'Species')
+
+#image
+install.packages('jpeg')
+library(jpeg)
+cat <- readJPEG('cat.jpeg')
+class(cat)
+dim(cat)
+r <- cat[,,1]
+g <- cat[,,2]
+b <- cat[,,3]
+cat.r.pca <- prcomp(r, center = F)
+cat.g.pca <- prcomp(g, center = F) 
+cat.b.pca <- prcomp(b, center = F)
+rgb.pca <- list(cat.r.pca, cat.g.pca, cat.b.pca)
+
+pc <- c(2,10,50,100,300)
+for (i in pc) {
+  pca.img <- sapply(rgb.pca, function(j) {
+    compressed.img <- j$x[,1:i] %*% t(j$rotation[,1:i])
+  }, simplify = 'array')
+  writeJPEG(pca.img, paste('cat_pca_', i, '.jpeg', sep = ''))
+}
